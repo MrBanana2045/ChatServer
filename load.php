fb96b7cc64cb7418d36a46a0131640b8e5bd6e9c
MrSaad <mrbananatyphon2020@gmail.com>
2025-09-04 00:52:58 +0330

    Update load.php

--- load.php
+++ load.php
@@ -1,6 +1,19 @@
 <?php
 header('Content-Type: application/json');
-
+function Lines($filename) {
+    if (!file_exists($filename)) {
+        return 0;
+    }
+    $file = new SplFileObject($filename, 'r');
+    $file->seek(PHP_INT_MAX);
+    $lineCount = $file->key() + 1;
+    
+    if ($lineCount > 0) {
+        $lineCount--;
+    }
+    
+    return $lineCount;
+}
 if (isset($_GET['group'])) {
     $group = $_GET['group'];
     $file = $group . '/server.txt';
@@ -19,12 +32,23 @@ if (isset($_GET['group'])) {
 $folders = scandir(getcwd());
 $groupNames = [];
 foreach($folders as $end) {
-    if ($end != '.' && $end != '..' && $end != 'save.php' && $end != 'load.php' && $end != 'index.html' && $end != 'user.php' && $end != 'users.json' && $end != 'server.php' && $end != 'users.json') { 
+    if ($end != '.' && $end != '..' && is_dir($end)) { 
         $groupNames[] = $end;
     }
 }
 
+$usersPerGroup = [];
+foreach ($groupNames as $group) {
+    $userFile = $group . '/users.txt';
+    if (file_exists($userFile)) {
+        $usersPerGroup[$group] = Lines($userFile);
+    } else {
+        $usersPerGroup[$group] = 0;
+    }
+}
+
 echo json_encode([
-    'groupName' => $groupNames
+    'groupName' => $groupNames,
+    'usersPerGroup' => $usersPerGroup
 ]);
 ?>
